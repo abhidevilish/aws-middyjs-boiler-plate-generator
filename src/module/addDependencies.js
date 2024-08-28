be3944b41bb64d7c83092fbbda3321c66d0245c9
unknown <aprasad543@gmail.com>
2024-08-28 19:30:18 +0530

    fixed auto formatting issue

--- src/module/addDependencies.js
+++ src/module/addDependencies.js
@@ -1,9 +1,11 @@
 const { execSync } = require('child_process');
+const delay = require('../utils/delay');
 
 module.exports = {
-    addDependencies: path => {
+    addDependencies:async path => {
         execSync(`npm i @middy/core @middy/event-normalizer  winston @middy/ssm --prefix ${path}`);
         execSync(`npm i jest aws-sdk-client-mock prettier --prefix ${path} --save-dev`);
+        await delay()
         execSync(`npm run format --prefix ${path}`);
     }
 }
